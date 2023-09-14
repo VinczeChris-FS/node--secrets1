c49c7d375acf65f58fddd918521ee6deb123975a
VinczeChris-FS <fs.cvincze@gmail.com>
2023-09-14 16:26:10 -0400

    Update env vars

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 // Hard coded environment variables
 PORT = 4000;
-const host = "localhost";
+HOST = "localhost";
 FIRST_NAME = "Chris";
 LAST_NAME = "Vincze";
 
@@ -12,6 +12,6 @@ app.get("/", (req, res) => {
   res.send(`${FIRST_NAME} ${LAST_NAME}'s Node Secrets Assignment`);
 });
 
-app.listen(port, host, () => {
-  console.log(`App listening on host ${host} port ${port}`);
+app.listen(PORT, HOST, () => {
+  console.log(`App listening on host ${HOST} port ${PORT}`);
 });
